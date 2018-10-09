2198b39765bd14f25de901643d9f11485ddfca4d
jiaxiaobo <jiaxiaobo@baidu.com>
2018-10-09 10:35:39 +0800

    test

---
+++ Project1/Filter.cpp
@@ -0,0 +1,17 @@
+#include "Filter.h"
+
+
+
+Filter::Filter()
+{
+}
+
+
+Filter::~Filter()
+{
+}
+
+void Filter::medianfilter(char * pic, char * smoothpic, int width, int height)
+{
+
+}
---
+++ Project1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>  
+#include <opencv2/core.hpp>  
+#include <opencv2/highgui.hpp>  
+
+using namespace cv;
+
+//
+
+int main()
+{
+/*
+	Mat img = imread("test.jpg");
+	namedWindow("sample");
+	imshow("sample", img);
+	waitKey(0);
+	*/
+
+	return 0;
+}
